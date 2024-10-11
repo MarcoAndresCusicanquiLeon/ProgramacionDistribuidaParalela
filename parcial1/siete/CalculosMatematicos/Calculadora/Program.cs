37725d613ab9ceda8e7fcc09cd1660c01b9541f3
MarcoAndresCusicanquiLeon <mcusicanquil@fcpn.edu.bo>
2024-10-10 23:43:28 -0400

    Add files via upload

---
+++ parcial1/siete/CalculosMatematicos/Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using CalculosMatematicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Calculadora con notación Prefija (Notación Polaca)---\n");
+
+            Console.WriteLine("------Ejemplo:------");
+            string expr = "+ 3 * 2,4 4,5";
+            Console.WriteLine(expr);
+            Console.WriteLine("da como resultado "+Operaciones.solve(expr)+"\n");
+
+            Console.WriteLine("Ingresa una expresion:");
+            expr=Console.ReadLine();
+            Console.WriteLine("Resultado: "+Operaciones.solve(expr));
+            Console.ReadLine();
+        }
+    }
+}
