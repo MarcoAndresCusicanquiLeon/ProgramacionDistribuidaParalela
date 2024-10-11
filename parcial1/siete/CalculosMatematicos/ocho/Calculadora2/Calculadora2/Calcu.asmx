1e2359c11d5512b3b2aeb1fb0b20e79581aa3c36
MarcoAndresCusicanquiLeon <mcusicanquil@fcpn.edu.bo>
2024-10-10 23:47:56 -0400

    Add files via upload

---
+++ parcial1/siete/CalculosMatematicos/ocho/Calculadora2/Calculadora2/Calcu.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Calcu.asmx.cs" Class="Calculadora2.Calcu" %>
---
+++ parcial1/siete/CalculosMatematicos/ocho/Calculadora2/Calculadora2/Calcu.asmx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Calculadora2
+{
+    /// <summary>
+    /// Descripción breve de Calcu
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calcu : System.Web.Services.WebService
+    {
+
+        [WebMethod(MessageName ="Calculadora notación Prefija")]
+        public double receive(string expresion)
+        {
+            return solve(expresion);
+        }
+
+        private double add(double a, double b)
+        {
+            return a + b;
+        }
+        private double sub(double a, double b)
+        {
+            return a - b;
+        }
+        private double mul(double a, double b)
+        {
+            return a * b;
+        }
+        private double div(double a, double b)
+        {
+            return a / b;
+        }
+
+        private double solve(string expr)
+        {
+            string[] decomposition = expr.Split();
+            Array.Reverse(decomposition);
+
+            Stack<double> nums_helper = new Stack<double>();
+            foreach (string piece in decomposition)
+            {
+                if (double.TryParse(piece, out double number))
+                {
+                    nums_helper.Push(number);
+                }
+                else
+                {
+                    if (nums_helper.Count < 2)
+                    {
+                        throw new InvalidOperationException("Se requieren al menos dos operandos para realizar la operación.");
+                    }
+                    double topnum1 = nums_helper.Pop();
+                    double topnum2 = nums_helper.Pop();
+
+                    double result;
+                    switch (piece)
+                    {
+                        case "+":
+                            result = add(topnum1, topnum2);
+                            break;
+                        case "-":
+                            result = sub(topnum1, topnum2);
+                            break;
+                        case "*":
+                            result = mul(topnum1, topnum2);
+                            break;
+                        case "/":
+                            result = div(topnum1, topnum2);
+                            break;
+                        default:
+                            throw new InvalidOperationException("Operador no válido");
+                    };
+
+                    nums_helper.Push(result);
+                }
+            }
+            if (nums_helper.Count > 1) throw new InvalidOperationException("Cantidad de números incorrecta");
+            return nums_helper.Pop();
+        }
+    }
+}
